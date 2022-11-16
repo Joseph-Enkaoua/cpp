5f871035b48c12faae75fca8fabfb09a44f7cd1a
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2022-11-16 19:27:17 +0100

    Update Converter.cpp

--- 06/ex00/Converter.cpp
+++ 06/ex00/Converter.cpp
@@ -27,10 +27,10 @@ void	Converter::findType(std::string str) {
 		this->charType(str[0]);
 	else if (isInt(str))
 		this->printValue();
-	// else if (isFloat(str))
-	// 	this->printValue();
-	// else if (isDouble(str))
-	// 	this->printValue();
+	else if (isFloat(str))
+		this->printValue();
+	else if (isDouble(str))
+		this->printValue();
 	else
         this->lastOne(str);
 }
@@ -38,7 +38,7 @@ void	Converter::findType(std::string str) {
 void	Converter::charType(char c) {
 	_char = c;
 	_int = static_cast<int>(c);
-	_float = static_cast<double>(c);
+	_float = static_cast<float>(c);
 	_double = static_cast<double>(c);
 	std::cout << "char: '" << _char << "'" << std::endl;
 	std::cout << "int: " << std::setprecision(1) << std::fixed << _int << std::endl;
@@ -59,8 +59,41 @@ bool	Converter::isInt(std::string str) {
 		throw Converter::invalidValueException();
 	_int = std::stoi(str);
     _char = static_cast<char>(_int);
-	_float = static_cast<double>(_int);
+	_float = static_cast<float>(_int);
 	_double = static_cast<double>(_int);
+	return true;
+}
+
+bool	Converter::isFloat(std::string str) {
+	int	dot = 0;
+	std::string	sub = str.substr(0, (str.length() - 1));
+	if (str[str.length() - 1] != 'f')
+		return false;
+	for (int i = 0;i < (int)sub.length();i++) {
+		if (sub[i] == '.')
+			dot++;
+		if ((!isnumber(sub[i]) && sub[i] != '.') || dot > 1)
+			return false;
+	}
+	_float = std::stof(sub);
+	_char = static_cast<char>(_float);
+	_int = static_cast<int>(_float);
+	_double = static_cast<double>(_float);
+	return true;
+}
+
+bool	Converter::isDouble(std::string str) {
+	int	dot = 0;
+	for (int i = 0;i < (int)str.length();i++) {
+		if (str[i] == '.')
+			dot++;
+		if ((!isnumber(str[i]) && str[i] != '.') || dot > 1)
+			return false;
+	}
+	_double = std::stod(str);
+	_char = static_cast<char>(_double);
+	_int = static_cast<int>(_double);
+	_float = static_cast<float>(_double);
 	return true;
 }
 
