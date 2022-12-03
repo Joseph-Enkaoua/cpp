5b32eec76911970518cff4c0f8e4dbf951c071b0
henkaoua <henkaoua@student.42lausanne.ch>
2022-12-03 21:00:39 +0100

    0702

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -3,35 +3,52 @@
 
 # include <iostream>
 # include <string>
+# include <exception>
+#include <ctime>
 
-template < typename T >
-class Array {
+template < typename T > class Array {
 private:
 	T				*var;
-	unsigned int	_n;
+	unsigned int	s;
 public:
-	Array() : var(NULL), _n(0) {}
-	Array(unsigned int n) : var(NULL), _n(n) {
+	Array() : var(new T[0]), s(0) {}
+	Array(unsigned int n) : var(new T[n]), s(n) {
 		if (n > 0) {
-			var = new T[n];
-			if (var == NULL)
-				
 			bzero(var, n);
 		} else {
-			std::cerr << "Error: number must be bigger than 0" << std::endl;
+			std::cerr << "Error: size must be bigger than 0" << std::endl;
 		}
 	}
-	Array   & operator=(const Array &rhs) {
-		this->var = rhs.var;
-		this->_n = rhs._n;
+	~Array() { delete[] var; }
+	Array(const Array &other) : var(new T[other.s]), s(other.s) {
+		for (int i = 0;i < (int)s;i++){
+			this->var[i] = other.var[i];
+		}
+	}
+	Array & operator=(const Array<T> &rhs) {
+		if (*this == rhs)
+			return *this;
+		delete[] var;
+		var = Array(rhs);
 		return *this;
 	}
-	Array(const Array &other) {
-		*this = other;
+	T	& operator[](unsigned int n) {
+		if (n >= s)
+			throw std::exception();
+		return (var[n]);
 	}
-	Array	& operator[](unsigned int n) {
-		return this->var[n];
-	}
+	unsigned int	size(){ return s; }
 };
+//for testing
+template < typename T > void printArray(Array<T>& arr, unsigned int length)
+{
+	for (unsigned int i = 0; i < length; i++)
+		std::cout << arr[i] << " ";
+}
 
+template < typename T > void printTypeArray(T& arr, unsigned int length)
+{
+	for (unsigned int i = 0; i < length; i++)
+		std::cout << arr[i] << " ";
+}
 #endif
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,52 +1,63 @@
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
-{
+#define MAX_VAL 20
+
+int main(void) {
+
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
+
     srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
+
+    for (int i = 0; i < MAX_VAL; i++) {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
-    //SCOPE
+    std::cout << "Test Array Generation" << std::endl
+                << "array[] numbers: ";
+    printArray(numbers, MAX_VAL); 
+    std::cout << std::endl
+                << "array[] mirror: ";
+    printTypeArray(mirror, MAX_VAL);
+    std::cout << std::endl << std::endl; 
+
+    // testing deep copy
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
+        std::cout << "Test Deep Copy" << std::endl
+                    << "array[] test: ";
+        printArray(test, MAX_VAL);
+        std::cout << std::endl << std::endl; 
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
             std::cerr << "didn't save the same value!!" << std::endl;
             return 1;
         }
     }
-    try
-    {
+
+    std::cout << "Trying to access array[-2]. Should return an exception." << std::endl; 
+    try {
         numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+    } catch(std::exception const& e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
+    std::cout << "Trying to access array[MAX_VALUE]. Should return an exception." << std::endl; 
+    try {
+        numbers[MAX_VAL] = 0;
+    } catch(std::exception const& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++) {
         numbers[i] = rand();
     }
-    delete [] mirror;//
+
+    delete[] mirror;
+
     return 0;
 }
