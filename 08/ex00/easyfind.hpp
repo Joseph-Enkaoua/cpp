c3e05431a5d5187804828d1cffc606df925622c1
henkaoua <henkaoua@student.42lausanne.ch>
2022-12-10 16:23:59 +0100

    0

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,9 +1,19 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP.
 
+# include <stdlib.h>
+# include <iostream>
+#include <iostream>
+#include <exception>
+#include <iterator>
+#include <vector>
+#include <list>
+#include <string>
+#include <deque>
+
 template < typename T >
-easyfind(T t, int i) {
-    
+int easyfind(T t, int i) {
+    return std::bsearch(t, i, );
 }
 
 #endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -1,6 +1,64 @@
 #include "easyfind.hpp"
 
-int main() {
-    
-    return 0;
+int main()
+{
+	std::vector<int> vector;
+
+	vector.push_back(1);
+	vector.push_back(9);
+	vector.push_back(13);
+
+	try
+	{
+		std::cout << "##Searching Through a Vector##" << std::endl;
+		std::cout << easyfind(vector, 1) << std::endl;
+		std::cout << easyfind(vector, 9) << std::endl;
+		std::cout << easyfind(vector, 15) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	std::list<char> my_list;
+
+	my_list.push_back('A');
+	my_list.push_back('L');
+	my_list.push_back('I');
+
+	try
+	{
+		std::cout << "##Searching Through a Doubly linked list##" << std::endl;
+		std::cout << easyfind(my_list, 65) << std::endl;
+		std::cout << easyfind(my_list, 76) << std::endl;
+		std::cout << easyfind(my_list, 15) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	std::deque<int> DQueue ;
+
+	DQueue.push_back(83);
+	DQueue.push_back(854);
+	DQueue.push_back(1047);
+
+	try
+	{
+		std::cout << "##Searching Through a Double Ended Queue##" << std::endl;
+		std::cout << easyfind(DQueue, (int)'S') << std::endl;
+		std::cout << easyfind(DQueue, 854) << std::endl;
+		std::cout << easyfind(DQueue, 10478) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	return 0;
 }
