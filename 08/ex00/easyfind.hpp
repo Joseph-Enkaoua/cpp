cef3154d65279c34e5f9d59f020f8df2ada68d2f
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2022-12-13 22:32:07 +0100

    0800.1

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,9 +1,22 @@
 #ifndef EASYFIND_HPP
-# define EASYFIND_HPP.
+# define EASYFIND_HPP
+
+# include <exception>
+# include <iostream>
+
+class notFoundException : public std::exception {
+public:
+    virtual const char* what() const throw() {
+        return ("Error 404: Not found");
+    }
+};
 
 template < typename T >
-easyfind(T t, int i) {
-    
+typename T::iterator easyfind(T c, int n) {
+    typename T::iterator i = std::find(c.begin(), c.end(), n);
+    if (i > c.end())
+        throw notFoundException();
+    return i;
 }
 
 #endif
